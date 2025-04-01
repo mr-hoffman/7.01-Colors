22ab996eef4695867f7800e6b2fccaac31c38262
mr-hoffman <hoffmant@hilbertk12.org>
2025-04-01 09:22:39 -0500

    Create color-class.py

---
+++ color-class.py
@@ -0,0 +1,18 @@
+################# Colors Class Lab ##################
+
+
+### define your Color class ###
+
+
+### define your add_color() function ###
+
+
+
+### Instantiate 3 Color objects ###
+
+
+
+### call your function and add some print() statements to test your code ###
+
+
+
